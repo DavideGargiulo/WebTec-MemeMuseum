5d4e76b2a56aebf292d7c522f823409eee58c0bd
DavideGargiulo <davide.gargiulo002@gmail.com>
2026-02-19 15:55:05 +0100

    feat: implemented method to navbar to change login text to the username of the user

--- frontend/src/app/_internalComponents/navbar/navbar.ts
+++ frontend/src/app/_internalComponents/navbar/navbar.ts
@@ -2,7 +2,10 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { LucideAngularModule, Search, Mic, User, Calendar } from 'lucide-angular';
+// Aggiunto LogOut alle icone
+import { LucideAngularModule, Search, Mic, User, Calendar, LogOut } from 'lucide-angular';
+// Importiamo il servizio di autenticazione
+import { AuthService } from '../../_services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,6 +16,7 @@ import { LucideAngularModule, Search, Mic, User, Calendar } from 'lucide-angular
 export class NavbarComponent {
   
   private router = inject(Router);
+  authService = inject(AuthService);
 
   searchQuery = '';
   searchDate = '';
@@ -21,7 +25,7 @@ export class NavbarComponent {
     Search, 
     Mic, 
     User,
-    Calendar
+    Calendar,
   };
 
   onSearch() {
@@ -32,12 +36,14 @@ export class NavbarComponent {
     }
 
     if (this.searchDate) {
-      // Usiamo la stessa data come inizio e fine per cercare in quel giorno specifico
       queryParams.startDate = this.searchDate;
       queryParams.endDate = this.searchDate;
     }
 
-    // Navighiamo verso la pagina di ricerca passando i parametri nell'URL
     this.router.navigate(['/search'], { queryParams });
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
